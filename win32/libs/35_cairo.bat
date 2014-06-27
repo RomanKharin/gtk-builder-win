1d94cd0b6aa5d127981b12b8597aa761d76744fd
Ignacio Casal Quinteiro <icq@gnome.org>
2014-06-27 10:17:36 +0200

    cairo: define _SSIZE_T_DEFINED on win32

--- win32/libs/35_cairo.bat
+++ win32/libs/35_cairo.bat
@@ -11,6 +11,8 @@ cd cairo-$MODVER
 echo Compile...
 
 ./configure --enable-win32=yes --enable-win32-font=yes --enable-png=yes --enable-ft=yes --enable-fc=yes --prefix=$PREFIX
+# this avoid an error building see: http://lists.cairographics.org/archives/cairo/2012-October/023675.html
+echo "#define _SSIZE_T_DEFINED 1" >> config.h
 make clean
 make 2>&1 | tee ../../logs/35_cairo-make.log
 make install 2>&1 | tee ../../logs/35_cairo-makeinstall.log
